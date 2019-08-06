9899b8dca839fd6a054ebbb3a8404267f0f2ebb1
Katriel <katrielsp046@gmail.com.br>
2019-08-06 19:03:31 -0300

    Teste

---
+++ src/course/Program.java
@@ -0,0 +1,32 @@
+package course;
+
+import java.util.Locale;
+
+public class Program {
+	public static void main(String[] args) {
+		String product1 = "Computer";
+		String product2 = "Office desk";
+
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measue = 53.234567;
+		Locale.setDefault(Locale.US);
+		System.out.println("Products:");
+		System.out.printf("%s,which price is $ %.2f %n", product1, price1);
+		System.out.printf("%s, wich price is $ %.2f %n", product2, price2);
+		System.out.println();
+		System.out.printf("Record: %d years old, code %d and gender: %c %n", age, code, gender);
+		System.out.println();
+		System.out.println("Alteracoes");
+		System.out.println("Alteaaaracoes");
+		System.out.printf("Measue with eight decimal places: %.8f %n", measue);
+		System.out.printf("Rouded (three decimal places): %.3f %n", measue);
+		System.out.printf("US decimal point: %.3f", measue);
+
+	}
+
+}
